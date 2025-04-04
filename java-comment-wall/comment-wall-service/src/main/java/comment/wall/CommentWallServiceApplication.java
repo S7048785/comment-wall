fbcf90e8d95e0f12c38cd7a893b6f5ea90726214
S7048785 <2889157408@qq.com>
2025-04-04 12:22:57 +0800

    项目初始化

---
+++ java-comment-wall/comment-wall-service/src/main/java/comment/wall/CommentWallServiceApplication.java
@@ -0,0 +1,13 @@
+package comment.wall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentWallServiceApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(CommentWallServiceApplication.class, args);
+	}
+	
+}
